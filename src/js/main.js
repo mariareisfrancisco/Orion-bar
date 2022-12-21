aa5e0b15d3d494667bdf795032f2d70576a328d6
mariareisfrancisco <maria.reis@studentfr.ch>
2022-12-21 14:41:06 +0100

    carousel fonctionne

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
 import Swiper, { Navigation, Pagination } from "swiper";
 
 Swiper.use([Navigation, Pagination]);
+
+const swiper = new Swiper(".swiper-container", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".carousel-next",
+    prevEl: ".carousel-prev",
+  },
+});
